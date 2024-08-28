4397a5e556f9436cdd0b8768f5f1bbb6c4ef2c13
Amal221200 <amalmurikkoli2000@gmail.com>
2024-08-28 17:01:44 +0530

    Flip Animations

--- src/components/UseAnimationControls.jsx
+++ src/components/UseAnimationControls.jsx
@@ -1,18 +1,24 @@
 import { motion, useAnimationControls } from "framer-motion"
 
 const UseAnimationControls = () => {
-  const controls = useAnimationControls()
+  const front = useAnimationControls()
+  const back = useAnimationControls()
 
   const handleClick = () => {
-    controls.start("flip")
+    front.start("flip")
+    back.start("flip")
   }
 
 
   return (
-    <div style={{ height: "100vh", display: "grid", placeContent: "center", gap: "0.8rem" }}>
+    <div style={{ height: "100vh", display: "grid", placeContent: "center", placeItems: 'center', gap: "0.8rem" }}>
       <button onClick={handleClick} className="example-button">Flip It!</button>
-      <motion.div style={{ width: '150px', height: '150px', background: 'black' }}
-        variants={{ initial: { rotate: '0deg' }, flip: { rotate: '360deg' } }} initial="initial" animate={controls} />
+      <div style={{ width: '150px', height: '150px', position: 'relative' }}>
+        <motion.div style={{ position: 'absolute', top: 0, left: 0, width: '150px', height: '150px', background: 'black', backfaceVisibility: 'hidden' }}
+          variants={{ initial: { rotateY: '0deg' }, flip: { rotateY: ['0deg', '180deg', '0deg'] } }} transition={{ duration: 2, times: [0, .5, 1] }} initial="initial" animate={front} />
+        <motion.div style={{ position: 'absolute', top: 0, left: 0, width: '150px', height: '150px', background: 'red', backfaceVisibility: 'hidden' }}
+          variants={{ initial: { rotateY: '180deg' }, flip: { rotateY: ['180deg', '0deg', '180deg'] } }} transition={{ duration: 2, times: [0, .5, 1] }} initial="initial" animate={back} />
+      </div>
     </div>
   )
 }
